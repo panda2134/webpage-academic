e63402cb881c838977b12aa870b33049ba52cb70
panda2134 <me@panda2134.site>
2024-06-26 00:23:49 +0800

    integrating nix flake build

--- flake.nix
+++ flake.nix
@@ -1,19 +1,35 @@
 {
-  description = "A very basic flake";
+  description = "Jiangyi's Academic Webpage";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    src = {
+      url = "git+file:.?submodules=1";
+      flake = false;  # not including this results in infinite recursion
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }: 
+  outputs = { self, src, nixpkgs, flake-utils }: 
     flake-utils.lib.eachDefaultSystem (system:
     let
       pkgs = nixpkgs.legacyPackages.${system};
+      nativeBuildInputs = [ pkgs.hugo pkgs.git pkgs.go ];
     in
     {
       devShells.default = pkgs.mkShell {
-        packages = [ pkgs.hugo pkgs.git pkgs.go ];
+        packages = nativeBuildInputs;
+      };
+        
+      packages.default = pkgs.stdenv.mkDerivation {
+        pname = "webpage-academic";
+        version = "1.0.0";
+        inherit src nativeBuildInputs;
+
+        buildPhase = ''
+          mkdir -p $out/var/www/
+          hugo --destination $out/var/www/
+        '';
       };
     }
     )
