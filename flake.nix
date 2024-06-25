b680227793838da84276365334e5efe3db53112f
panda2134 <me@panda2134.site>
2024-06-26 00:56:02 +0800

    flake compatibility
    now use "nix build .?submodule=1" to build

--- flake.nix
+++ flake.nix
@@ -4,13 +4,9 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    theme-papermod = {
-      url = "git+file:themes/PaperMod?shallow=1"; 
-      flake = false; # avoid infinite recursion
-    };
   };
 
-  outputs = { self, nixpkgs, theme-papermod, flake-utils }: 
+  outputs = { self, nixpkgs, flake-utils }: 
     flake-utils.lib.eachDefaultSystem (system:
     let
       pkgs = nixpkgs.legacyPackages.${system};
@@ -29,7 +25,6 @@
 
         buildPhase = ''
           mkdir -p $out/var/www/
-          ${pkgs.rsync}/bin/rsync -rl ${theme-papermod} themes/PaperMod/
           hugo --destination $out/var/www/
         '';
       };
